096d97d6e72d391ce736accaadde8c3b7d73ca6d
chenz <chenz@coracle.com>
2018-12-20 14:07:15 +0800

    部署开始

---
+++ manager/entity/src/main/java/com/coracle/entity/TgTransOtherOrder.java
@@ -0,0 +1,93 @@
+package com.coracle.entity;
+
+import java.util.Date;
+
+public class TgTransOtherOrder {
+    //主键id
+    private Long id ;
+    //订单id
+    private Long orderId ;
+    //创建人
+    private Long createdBy;
+    //创建时间
+    private Date createdDate;
+    //最后修改人
+    private Long lastUpdatedBy;
+    //最后修改时间
+    private Date lastUpdatedDate;
+    private Long orgId;		//所在组织Id
+    private String name;		//所在组织Id
+    private String orgName;		//所在组织Id
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Long orderId) {
+        this.orderId = orderId;
+    }
+
+    public Long getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(Long createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public Long getLastUpdatedBy() {
+        return lastUpdatedBy;
+    }
+
+    public void setLastUpdatedBy(Long lastUpdatedBy) {
+        this.lastUpdatedBy = lastUpdatedBy;
+    }
+
+    public Date getLastUpdatedDate() {
+        return lastUpdatedDate;
+    }
+
+    public void setLastUpdatedDate(Date lastUpdatedDate) {
+        this.lastUpdatedDate = lastUpdatedDate;
+    }
+
+    public Long getOrgId() {
+        return orgId;
+    }
+
+    public void setOrgId(Long orgId) {
+        this.orgId = orgId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getOrgName() {
+        return orgName;
+    }
+
+    public void setOrgName(String orgName) {
+        this.orgName = orgName;
+    }
+}
